56d549d38694d6f362a663fc316883f5915cd418
sergiohdljr <sergio.tjf1@gmail.com>
2022-07-28 09:41:10 -0300

    ajustes layout e adicionado msg de erro com javascript

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const botao = document.querySelector('.btn-input');
+const body = document.querySelector("body");
 
 botao.addEventListener('click',(evento)=>{
     evento.preventDefault();
@@ -8,29 +9,36 @@ botao.addEventListener('click',(evento)=>{
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     
       let validar = re.test(valorInput)
-      
-})
-    
+
+      if (validar == true){
+        const email = document.querySelector(".input-value");
+        email.value = "";
+      }else{
+        Invalido();
+        }
+      })
 
      function Invalido(){
-
         const erro = document.createElement('span')
         erro.innerText = "Please provide a valid e-mail"
         erro.classList.add("span-erro")
 
         const input = document.querySelector(".input");
         input.classList.add("input-erro");
-
-        const main = document.querySelector('.principal')
-
-        main.appendChild(erro) 
+ 
         const email = document.querySelector(".input-value");
 
         const errorIcon = document.createElement('img');
         errorIcon.setAttribute("src","./images/icon-error.svg")
         errorIcon.classList.add("icon-error");
 
-        input.insertBefore(errorIcon, input.children[1]);
+        const textos = document.querySelector('.textos')
+
+        const form = document.querySelector(".formulario")
         
+        form.appendChild(erro);
+
+        input.insertBefore(errorIcon, input.children[1]);
+
         email.value = " "
-    }
+     }
