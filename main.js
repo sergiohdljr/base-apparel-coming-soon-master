20c682ef8255f7620061f21193aca2bfc93945bc
sergiohdljr <sergio.tjf1@gmail.com>
2022-07-29 18:11:41 -0300

    alteração mensagem de erro

--- main.js
+++ main.js
@@ -1,44 +1,38 @@
-const botao = document.querySelector('.btn-input');
-const body = document.querySelector("body");
+const input = document.querySelector(".input");
+const botao = document.querySelector(".btn-input");
+const email = document.querySelector(".input-value");
+const form = document.querySelector(".formulario");
 
-botao.addEventListener('click',(evento)=>{
-    evento.preventDefault();
-    const email = document.querySelector('.input-value')
-    const valorInput = email.value;
-    var re =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    
-      let validar = re.test(valorInput)
+const erro = document.createElement("span");
+erro.innerText = "Please provide a valid e-mail";
 
-      if (validar == true){
-        const email = document.querySelector(".input-value");
-        email.value = "";
-      }else{
-        Invalido();
-        }
-      })
+const errorIcon = document.createElement("img");
+errorIcon.setAttribute("src", "./images/icon-error.svg");
+errorIcon.classList.add("icon-error");
 
-     function Invalido(){
-        const erro = document.createElement('span')
-        erro.innerText = "Please provide a valid e-mail"
-        erro.classList.add("span-erro")
+botao.addEventListener("click", (evento) => {
+  evento.preventDefault();
+  const email = document.querySelector(".input-value");
+  const valorInput = email.value;
+  var re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-        const input = document.querySelector(".input");
-        input.classList.add("input-erro");
- 
-        const email = document.querySelector(".input-value");
+  let validar = re.test(valorInput);
 
-        const errorIcon = document.createElement('img');
-        errorIcon.setAttribute("src","./images/icon-error.svg")
-        errorIcon.classList.add("icon-error");
+  if (validar == true) {
+    email.value = "";
+    input.classList.remove("input-erro");
+    form.removeChild(erro);
+    input.removeChild(errorIcon)
 
-        const textos = document.querySelector('.textos')
+  } else {
+    Invalido();
+  }
+});
 
-        const form = document.querySelector(".formulario")
-        
-        form.appendChild(erro);
-
-        input.insertBefore(errorIcon, input.children[1]);
-
-        email.value = " "
-     }
+function Invalido() {
+  erro.classList.add("span-erro");
+  input.classList.add("input-erro");
+  form.appendChild(erro);
+  input.insertBefore(errorIcon, input.children[1]);
+}
