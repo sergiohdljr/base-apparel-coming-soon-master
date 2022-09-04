4f1fdc1ec23a5104b5cfb5bd4b1e5e7942ba4124
sergiohdljr <sergio.tjf1@gmail.com>
2022-09-04 18:00:29 -0300

    refatorar

--- main.js
+++ main.js
@@ -2,29 +2,24 @@ const input = document.querySelector(".input");
 const botao = document.querySelector(".btn-input");
 const email = document.querySelector(".input-value");
 const form = document.querySelector(".formulario");
-
 const erro = document.createElement("span");
 erro.innerText = "Please provide a valid e-mail";
-
 const errorIcon = document.createElement("img");
 errorIcon.setAttribute("src", "./images/icon-error.svg");
 errorIcon.classList.add("icon-error");
 
 botao.addEventListener("click", (evento) => {
   evento.preventDefault();
-  const email = document.querySelector(".input-value");
-  const valorInput = email.value;
-  var re =
+  
+  const re =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  let validar = re.test(valorInput);
-
-  if (validar == true) {
+  const validar = re.test(email.value);
+  
+  if (validar) {
     email.value = "";
     input.classList.remove("input-erro");
     form.removeChild(erro);
-    input.removeChild(errorIcon)
-
+    input.removeChild(errorIcon);
   } else {
     Invalido();
   }
